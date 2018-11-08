d1eb7ef31e873cf07773f482f9e18c01d048e060
asulbaran <asulbaran@panodi.com>
2018-11-08 15:30:14 -0400

    Updates

---
+++ precache-manifest.ecb12ed455ee38af92971c228fe30c00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9cd93de0834cc9f18e8",
+    "url": "/react-pokedex/static/js/runtime~main.f9cd93de.js"
+  },
+  {
+    "revision": "104c3a6f1786a33484f0",
+    "url": "/react-pokedex/static/js/main.104c3a6f.chunk.js"
+  },
+  {
+    "revision": "6297498819435d23e00d",
+    "url": "/react-pokedex/static/js/1.62974988.chunk.js"
+  },
+  {
+    "revision": "104c3a6f1786a33484f0",
+    "url": "/react-pokedex/static/css/main.5c283649.chunk.css"
+  },
+  {
+    "revision": "f1a25fe2ee24d101dd80e727c9ff6d14",
+    "url": "/react-pokedex/index.html"
+  }
+];
