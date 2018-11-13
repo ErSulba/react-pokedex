7711aa585e163a74bac2bf59ddf1f9bde6e581ba
asulbaran <asulbaran@panodi.com>
2018-11-13 16:03:06 -0400

    style added

--- src/componets/pokemon/components/search.js
+++ src/componets/pokemon/components/search.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import styles from './search.module.scss'
 export default function Search({handleOnchange}) {
   return (
-    <div>
+    <div className={styles.search}>
+      <div>
       <input onChange={handleOnchange} placeholder='search pokemon by dex number or name' />
+      </div>
     </div>
   )
 }
